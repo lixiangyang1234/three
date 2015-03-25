dd55f24387f478ecd05cba819776b5120cbae9a1
YY <jch@jchdeMac-mini.local>
2015-03-25 17:17:59 +0800

    Initial Commit

---
+++ ThreeMan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ThreeMan
+//
+//  Created by YY on 15-3-25.
+//  Copyright (c) 2015年 ___普马克___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
