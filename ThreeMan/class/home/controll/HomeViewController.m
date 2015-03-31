0288f75cfa95a125dccf930d843cc12cfc4ac701
YY <jch@jchdeMac-mini.local>
2015-03-31 16:23:07 +0800

    123

--- ThreeMan/class/home/controll/HomeViewController.m
+++ ThreeMan/class/home/controll/HomeViewController.m
@@ -13,7 +13,7 @@
 #import "NeedViewController.h"
 #import "BusinessController.h"
 #define BANNER    135           //banner高度
-#define EIGHTH    125          //八大课程体系
+#define EIGHTH    95          //八大课程体系
 #define NEEDH     135         //按需求
 #define BUSINESS  183       //按行业
 #define NEEDTAG  100
@@ -38,12 +38,12 @@ static NSString * const reuseIdentifier = @"Cell";
     [_bannerArray addObject:@"img"];
     [_bannerArray addObject:@"img"];
     [_bannerArray addObject:@"img"];
-
+    
     [self addUIBanner];//1区
     [self addUICourse];//2区添加八大课程体系
     [self addUINeed];//3添加需求
     [self addUIBusiness];//添加行业
-
+    
 }
 //1区
 -(void)addUIBanner{
@@ -51,7 +51,7 @@ static NSString * const reuseIdentifier = @"Cell";
     self.backScrollView =[[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, kWidth, kHeight-104)];
     self.backScrollView.backgroundColor =[UIColor whiteColor];
     [self.view addSubview:self.backScrollView];
-    self.backScrollView.contentSize =CGSizeMake(kWidth, 714);
+    self.backScrollView.contentSize =CGSizeMake(kWidth, 592+EIGHTH);
     self.backScrollView.bounces=NO;
     self.backScrollView.showsHorizontalScrollIndicator =NO;
     self.backScrollView.showsVerticalScrollIndicator=NO;
@@ -70,7 +70,7 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 //2八大课程体系
 -(void)addUICourse{
-
+    
     
     self.noticeArray=@[@"企业蜕变",@"领袖蜕变",@"跟进服务",@"协作突破",@"总裁实修",@"心灵修炼",@"能力构建",@"生命辅修"];
     for (int c=0; c<self.noticeArray.count; c++)
@@ -78,10 +78,10 @@ static NSString * const reuseIdentifier = @"Cell";
         CGFloat courseBorderW = 35;//边界宽
         CGFloat courseBorderH = 9;//边界高
         CGFloat betweenW = 39;// 间距宽
-
+        CGFloat imageWith =(kWidth-courseBorderW*2-betweenW *3)/4; //图片宽
         
         UIImageView *courseImage =[[UIImageView alloc]init];
-        courseImage.frame =CGRectMake(courseBorderW+c%4*(betweenW+(kWidth-courseBorderW*2-betweenW*3)/4),courseBorderH+ BANNER+c/4*(23+courseBorderH+(kWidth-courseBorderW*2-betweenW*3)/4), (kWidth-courseBorderW*2-betweenW*3)/4,(kWidth-courseBorderW*2-betweenW*3)/4);
+        courseImage.frame =CGRectMake(courseBorderW+c%4*(betweenW+imageWith),courseBorderH+ BANNER+c/4*(23+courseBorderH+(kWidth-courseBorderW*2-betweenW*3)/4), imageWith,imageWith);
         [courseImage setBackgroundColor:[UIColor cyanColor]];
         courseImage.layer.cornerRadius =(kWidth-courseBorderW*2-betweenW*3)/8;
         courseImage.layer.masksToBounds=YES;
@@ -90,7 +90,7 @@ static NSString * const reuseIdentifier = @"Cell";
         
         UIButton *courseButtTitle =[UIButton buttonWithType:UIButtonTypeCustom];
         
-        courseButtTitle.frame =CGRectMake(26+c%4*(courseImage.frame.size.width+betweenW), courseImage.frame.origin.y+45+c/4, 70, 30);
+        courseButtTitle.frame =CGRectMake(18+c%4*(courseImage.frame.size.width+betweenW), courseImage.frame.origin.y+30+c/4, 70, 30);
         [self.backScrollView addSubview:courseButtTitle];
         [courseButtTitle setTitle:self.noticeArray[c] forState:UIControlStateNormal];
         [courseButtTitle setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
@@ -100,19 +100,20 @@ static NSString * const reuseIdentifier = @"Cell";
         
         
         UIButton *courseBtn =[UIButton buttonWithType:UIButtonTypeCustom];
-        courseBtn.frame =CGRectMake(courseBorderW+c%4*(courseButtTitle.frame.size.width+15), courseImage.frame.origin.y+c/4, 50,70);
+        courseBtn.frame =CGRectMake(30+c%4*(courseButtTitle.frame.size.width), courseImage.frame.origin.y+c/4, 50,60);
         [courseBtn setTitle:self.noticeArray[c] forState:UIControlStateNormal  ];
         [courseBtn setTitleColor:[UIColor clearColor] forState:UIControlStateNormal];
         [_backScrollView addSubview:courseBtn];
-        courseBtn.backgroundColor =[UIColor clearColor];
-//        CategoryButt.tag=[hotCategoryModel.cateid intValue]+100;
+        //        courseBtn.backgroundColor =[UIColor yellowColor];
+        //        courseBtn.alpha =.4;
+        //        CategoryButt.tag=[hotCategoryModel.cateid intValue]+100;
         
         courseImage.tag = courseBtn.tag+10000;
         courseButtTitle.tag =courseImage.tag ;
         
         courseImage.userInteractionEnabled = NO;
         courseImage.image =[UIImage imageNamed:@"nav_return_pre"];
-//        [findImage setImageWithURL:[NSURL URLWithString:hotCategoryModel.image]  placeholderImage:[UIImage imageNamed:@"find_fail.png"]];
+        //        [findImage setImageWithURL:[NSURL URLWithString:hotCategoryModel.image]  placeholderImage:[UIImage imageNamed:@"find_fail.png"]];
         
         [courseBtn addTarget:self action:@selector(eightCourseBtnClick:) forControlEvents:UIControlEventTouchUpInside];
         
@@ -120,8 +121,8 @@ static NSString * const reuseIdentifier = @"Cell";
         
         
     }
-
-
+    
+    
 }
 //2、八大课程体系按钮
 -(void)eightCourseBtnClick:(UIButton *)cate{
@@ -167,40 +168,41 @@ static NSString * const reuseIdentifier = @"Cell";
     needBackView.backgroundColor =HexRGB(0xeaebec);
     
     NSArray *needTitleArray =@[@"九大项",@"三大委员会",@"客户管理",@"外勤管理",@"内勤管理",@"营销活动"];
-        for (int i=0; i<6; i++) {
-            UIButton* needBtn =[UIButton buttonWithType:UIButtonTypeCustom];
-            [self.backScrollView addSubview:needBtn];
-            [needBtn setImage:[UIImage imageNamed:@"nav_return_pre"] forState:UIControlStateNormal];
-            [needBtn setTitle:needTitleArray[i] forState:UIControlStateNormal];
-            [needBtn setTitleColor:HexRGB(0x808080) forState:UIControlStateNormal];
-            needBtn.backgroundColor =[UIColor whiteColor];
-            if (i==0) {
-                needBtn.frame =CGRectMake(0, needHeight+40, kWidth-150, 90);
-            }else if (i==1){
-                needBtn.frame =CGRectMake(kWidth-149, needHeight+40, 150, 44);
-            }else if (i==2){
-                needBtn.frame =CGRectMake(kWidth-149, needHeight+40+45, 150, 45);
-
-            }else{
-                needBtn.frame =CGRectMake(i%3*(kWidth/3+1), needHeight+40+91, kWidth/3, 45);
-            }
-            [needBtn addTarget:self action:@selector(needBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+    for (int i=0; i<6; i++) {
+        UIButton* needBtn =[UIButton buttonWithType:UIButtonTypeCustom];
+        [self.backScrollView addSubview:needBtn];
+        [needBtn setImage:[UIImage imageNamed:@"nav_return_pre"] forState:UIControlStateNormal];
+        [needBtn setTitle:needTitleArray[i] forState:UIControlStateNormal];
+        [needBtn setTitleColor:HexRGB(0x808080) forState:UIControlStateNormal];
+        needBtn.backgroundColor =[UIColor whiteColor];
+        if (i==0) {
+            needBtn.frame =CGRectMake(0, needHeight+40, kWidth-150, 90);
+        }else if (i==1){
+            needBtn.frame =CGRectMake(kWidth-149, needHeight+40, 150, 44);
+        }else if (i==2){
+            needBtn.frame =CGRectMake(kWidth-149, needHeight+40+45, 150, 45);
             
-            needBtn.tag =i+NEEDTAG;
-
+        }else{
+            needBtn.frame =CGRectMake(i%3*(kWidth/3+1), needHeight+40+91, kWidth/3, 45);
+        }
+        [needBtn addTarget:self action:@selector(needBtnClick:) forControlEvents:UIControlEventTouchUpInside];
+        
+        needBtn.tag =i+NEEDTAG;
+        
     }
     
     
-
+    
 }
 //4、添加行业
 -(void)addUIBusiness{
-    UIView *needBackView =[[UIView alloc]initWithFrame:CGRectMake(0, NEEDH+BANNER+249, kWidth, BUSINESS)];
+    UIView *needBackView =[[UIView alloc]initWithFrame:CGRectMake(0, NEEDH+BANNER+EIGHTH+127, kWidth, BUSINESS)];
     [self.backScrollView addSubview:needBackView];
     needBackView.backgroundColor =HexRGB(0xeaebec);
+    //    needBackView.backgroundColor =[UIColor redColor];
     for (int i=0; i<6; i++) {
         UIButton* businessBtn =[UIButton buttonWithType:UIButtonTypeCustom];
-                    businessBtn.frame =CGRectMake(0+i/3*(kWidth/2+1),NEEDH+ BANNER+250+i%3*61, (kWidth)/2, 60 );
+        businessBtn.frame =CGRectMake(0+i/3*(kWidth/2+1),NEEDH+ BANNER+EIGHTH+128+i%3*61, (kWidth)/2, 60 );
         [self.backScrollView addSubview:businessBtn];
         [businessBtn setImage:[UIImage imageNamed:@"nav_return_pre"] forState:UIControlStateNormal];
         [businessBtn setTitle:@"课程推荐" forState:UIControlStateNormal];
@@ -212,11 +214,11 @@ static NSString * const reuseIdentifier = @"Cell";
         businessBtn.tag =i+10;
         
     }
-
+    
 }
 //3、需求
 -(void)needBtnClick:(UIButton *)more{
-
+    
     if (more.tag==NEEDTAG) {
         NineBlockController *nineVc =[[NineBlockController alloc]init];
         [self.nav pushViewController:nineVc animated:YES];
@@ -227,7 +229,7 @@ static NSString * const reuseIdentifier = @"Cell";
         NeedViewController *needVc =[[NeedViewController alloc]init];
         [self.nav pushViewController:needVc animated:YES];
     }
-       }
+}
 //行业
 -(void)businessBtnClick:(UIButton *)sender{
     BusinessController *businessVC=[[BusinessController alloc]init];
@@ -257,10 +259,10 @@ static NSString * const reuseIdentifier = @"Cell";
 //选中广告的第几张图片
 - (void)cycleBannerView:(KDCycleBannerView *)bannerView didSelectedAtIndex:(NSUInteger)index
 {
-//    AdsItem *item = [_adsArray objectAtIndex:index];
-//    BannerDetailController *detail = [[BannerDetailController alloc] init];
-//    detail.urlStr = item.content;
-//    [self.navigationController pushViewController:detail animated:YES];
+    //    AdsItem *item = [_adsArray objectAtIndex:index];
+    //    BannerDetailController *detail = [[BannerDetailController alloc] init];
+    //    detail.urlStr = item.content;
+    //    [self.navigationController pushViewController:detail animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -269,13 +271,13 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 
 /*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
 
 @end
